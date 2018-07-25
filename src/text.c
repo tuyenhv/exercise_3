a3c0673ec7c22af3ff99c31c0583931baafb55e5
Hoang Van Tuyen <tuyen.hoangvan@toshiba-tsdv.com>
2018-07-25 15:09:37 +0700

    Add list files for structure of source code
    Signed-off-by: Hoang Van Tuyen <tuyen.hoangvan@toshiba-tsdv.com>

---
+++ src/text.c
@@ -0,0 +1,5 @@
+/*******************************************************************************
+ * text.c file
+ * This file adds functions for adding new words with insertion or not
+*******************************************************************************/
+#include "include/text.h"
