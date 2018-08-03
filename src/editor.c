460fdef2e5c884cadaca19e96edf31254e984bb2
Hoang Van Tuyen <tuyen.hoangvan@toshiba-tsdv.com>
2018-08-03 10:29:17 +0700

    termios.h: Do not duplicate including the lib
    We include this lib in common.h file.
    Do not need to include it again in the *.c file.
    
    Signed-off-by: Hoang Van Tuyen <tuyen.hoangvan@toshiba-tsdv.com>

--- src/editor.c
+++ src/editor.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <termios.h>
 #include <unistd.h>
 #include "../include/common.h"
 #include "../include/window.h"
--- src/terminal_modes.c
+++ src/terminal_modes.c
@@ -2,7 +2,6 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <termios.h>
 #include <unistd.h>
 #include "../include/common.h"
 
