0b605373da7a9f1b320a24e713157c42f47e1262
Hoang Van Tuyen <tuyen.hoangvan@toshiba-tsdv.com>
2018-08-03 09:36:29 +0700

    terminal_modes: Add comments for flags in terminal mode
    Signed-off-by: Hoang Van Tuyen <tuyen.hoangvan@toshiba-tsdv.com>

--- src/terminal_modes.c
+++ src/terminal_modes.c
@@ -16,12 +16,30 @@ void enable_rawmode(void){
   if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
   atexit(disable_rawmode);
   struct termios raw = E.orig_termios;
+  
+  /* Explanation for features with each flag: **********************************
+   * The below is flags and the meaning will change from on to off if we change
+   * the value of flags.
+   * ECHO: Print what you typed to the terminal.
+   * ICANON: Allow us to turn off canonical mode. (read input byte by byte instead
+   *         of line by line).
+   * ISIG: Turn off/on the sending of both of these signals. (SIGINT and SIGTSTP)
+   * IXON: Control pausing and resuming the transmission Ctrl-S and Ctrl-Q.
+   * IEXTEN: Disable Ctrl-V
+   * ICRNL: Fix Ctrl-M
+   * OPOST: Turn off all output.
+   * BRKINT: a break condition will cause a SIGINT signal to be sent to the program
+   * like pressing Ctrl-C.
+   * INPCK: Enable parity checking.
+   * ISTRIP: causes the 8th bit of each input byte to be stripped.
+   * CS8: is not a flag. It sets the character size (CS) to 8 bits per byte.
+   ****************************************************************************/
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
-  //raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-  raw.c_lflag &= ~(ICANON | IEXTEN | ISIG);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
+
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
